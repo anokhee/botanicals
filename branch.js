4476020b8e8912389bc678a2bce50295d1ce9cb8
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2020-04-16 19:30:46 -0400

    Gets basic l-systems stem with randomization

--- branch.js
+++ branch.js
@@ -1,27 +1,20 @@
-function Branch(parent, pos, dir) {
-    this.pos = pos;
-    this.parent = parent;
-    this.dir = dir;
-    this.origDir = this.dir.copy();
-    this.count = 0;
-    this.len = 5;
-  
-    this.reset = function() {
-      this.dir = this.origDir.copy();
-      this.count = 0;
-    };
-  
-    this.next = function() {
-      var nextDir = p5.Vector.mult(this.dir, this.len);
-      var nextPos = p5.Vector.add(this.pos, nextDir);
-      var nextBranch = new Branch(this, nextPos, this.dir.copy());
-      return nextBranch;
-    };
-  
-    this.show = function() {
-      if (parent != null) {
-        stroke(255);
-        line(this.pos.x, this.pos.y, this.parent.pos.x, this.parent.pos.y);
-      }
-    };
-  }
+function branch() {
+    len *= Math.random() * (.52 - .45) + .45;
+    var nextSentence = '';
+    for (var i = 0; i < sentence.length; i++) {
+        var current = sentence.charAt(i);
+        var found = false;
+        for (var j = 0; j < rules.length; j++) {
+            if (current == rules[j].a) {
+                found = true;
+                nextSentence += rules[j].b;
+                break;
+            }
+        }
+        if (!found) {
+            nextSentence += current;
+        }
+    }
+    sentence = nextSentence;
+    turtle();
+}
--- index4.js
+++ index4.js
@@ -1,69 +1,48 @@
 var angle;
 var axiom = 'F';
 var sentence = axiom;
-var len = Math.random() * (150 - 120) + 120;
+var len = Math.random() * (350 - 220) + 220;
 
-var arr = ['a'];
+var count = 0;
+var arr = [];
+
+var iterations = Math.floor(Math.random() * (8 - 4) + 4);
 
 var rules = [];
-// rules[0] = {
-//     a: 'F',
-//     b: 'G[+F][-F]GF'
-// };
-
-// rules[1] = {
-//     a: 'G',
-//     b: 'GG'
-// };
-
 rules[0] = {
     a: 'F',
-    b: 'FF[+F][-F]'
+    b: 'G[+F][-F]GF'
 };
 
-
-
-function generate() {
-    console.log(arr);
-    len *= 0.5;
-    var nextSentence = '';
-    for (var i = 0; i < sentence.length; i++) {
-        var current = sentence.charAt(i);
-        var found = false;
-        for (var j = 0; j < rules.length; j++) {
-            if (current == rules[j].a) {
-                found = true;
-                nextSentence += rules[j].b;
-                break;
-            }
-        }
-        if (!found) {
-            nextSentence += current;
-        }
-    }
-    sentence = nextSentence;
-    createP(sentence);
-    turtle();
-}
+rules[1] = {
+    a: 'G',
+    b: 'GG'
+};
 
 function turtle() {
-    strokeWeight(1);
+   
+    angle = radians(Math.random() * (25 - 15) + 15);
+    noStroke();
     background(250);
+    
     resetMatrix();
     translate(width / 2, height);
     stroke(15);
     for (var i = 0; i < sentence.length; i++) {
+        finished = false;
         var current = sentence.charAt(i);
-        var randomSeed = Math.random() * 6;
+        var randomSeed = Math.random() * (2.5 - (-2.5) + -2.5);
         if (current == 'F' || current == 'G') {
             strokeWeight(1);
             line(0, 0, 0, -len - Math.random() * randomSeed);
-            // ellipse(0, -len, 1);  
+            // ellipse(0, -len + Math.random() * 15, 3);
+            arr.push([0, -len]);
             translate(0, -len);
+            count++;
         } else if (current == '+') {
-            rotate(angle * Math.random() * 2);
+            rotate(angle * Math.random() * randomSeed);
         } else if (current == '-') {
-            rotate(-angle * Math.random() * 2);
+            rotate(-angle * Math.random() * randomSeed);
         } else if (current == '[') {
             push();
         } else if (current == ']') {
@@ -72,12 +51,22 @@ function turtle() {
     }
 }
 
+function generateStems(iterations) {
+    for (i = 0; i < iterations; i++) {
+        turtle();
+        branch();
+    }
+}
+
 function setup() {
-    createCanvas(400, 400);
-    angle = radians(25);
+    createCanvas(600, 600);
     background(51);
-    createP(axiom);
     turtle();
-    var button = createButton('generate');
-    button.mousePressed(generate);
+}
+
+function draw() {
+    generateStems(iterations);
+    console.log(count, arr.length, sentence.length);
+    console.log(arr);
+    noLoop();
 }
