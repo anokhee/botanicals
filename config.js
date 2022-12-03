aa0f4c0e9675a393ed12433737e6f0f69ddea9e7
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro-2.local>
2022-12-03 17:10:51 -0500

    Good boilerplate -- committing for future ref

--- config.js
+++ config.js
@@ -1,56 +1,56 @@
 var baseColor = {
-    r: Math.random() * 200,
-    g: Math.random() * 200,
-    b: Math.random() * 205
-}
+  r: Math.random() * 100,
+  g: Math.random() * 100,
+  b: Math.random() * 100,
+};
 
 var mainConfig = {
-    flowers: {
-        color: {
-            r: baseColor.r,
-            g: baseColor.g,
-            b: baseColor.b,
-            brightOff: {
-                max: 200,
-                min: -200
-            },
-            colorOff: Math.random() * (100 - -60) + -60,
-            pistilColor: [baseColor.r + 200, baseColor.g + 200, baseColor.b + 55],
-        },
-        scale: {
-            min: 10,
-            max: 25
-        },
-        density: 1,
-        petalControlPoints: {
-            cp1x: Math.random() * (45 - (-45) + -45),
-            cp1y: 0,
-            cp2x: Math.random() * (35 - (-30) + -30),
-            cp2y: Math.random() * (55 - (-55) + -55),
-            cp3x: Math.random() * (35 - (-30) + -30),
-            cp3y: Math.random() * (0 - (-35) + -35),
-            cp4x: 0,
-            cp4y: 0,
-        },
-        petalCount: {
-            min: 1,
-            max: 4,
-            randomOffset: 1
-        },
-        pistilRadius: Math.random() * 30,
+  flowers: {
+    color: {
+      r: baseColor.r,
+      g: baseColor.g,
+      b: baseColor.b,
+      brightOff: {
+        max: 100,
+        min: 50,
+      },
+      colorOff: Math.random() * (100 - -60) + -60,
+      pistilColor: [baseColor.r + 255, baseColor.g + 255, baseColor.b + 255],
     },
-    stems: {
-        angle : {
-            max: 32,
-            min: 18
-        },
-        color: {
-            r: Math.random() * (120 - 90) + 90,
-            g: Math.random() * (120 - 90) + 90,
-            b: Math.random() * (80)
-        },
-        count: Math.floor(Math.random() * (8 - 6) + 6),
-        randomSeed: Math.floor(Math.random() * (3 - 2.25) + 2.25),
-        strokeWeight: 1.25
-    }
-}
+    scale: {
+      min: 20,
+      max: 20,
+    },
+    density: 1,
+    petalControlPoints: {
+      cp1x: Math.random() * (45 - -45 + -45),
+      cp1y: 0,
+      cp2x: Math.random() * (35 - -30 + -30),
+      cp2y: Math.random() * (55 - -55 + -55),
+      cp3x: Math.random() * (35 - -30 + -30),
+      cp3y: Math.random() * (0 - -35 + -35),
+      cp4x: 0,
+      cp4y: 0,
+    },
+    petalCount: {
+      min: 1,
+      max: 5,
+      randomOffset: 5,
+    },
+    pistilRadius: Math.random() * 30,
+  },
+  stems: {
+    angle: {
+      max: 32,
+      min: 18,
+    },
+    color: {
+      r: Math.random() * (120 - 90) + 90,
+      g: Math.random() * (120 - 90) + 90,
+      b: Math.random() * 80,
+    },
+    count: Math.floor(Math.random() * (8 - 6) + 6),
+    randomSeed: Math.floor(Math.random() * (3 - 2.25) + 2.25),
+    strokeWeight: 1.25,
+  },
+};
--- index.js
+++ index.js
@@ -1,103 +1,67 @@
-const axiom = 'F';
-let w = window.innerWidth;
-let h = window.innerHeight
-let len = Math.random() * ((h / 2.15) - h / 2.6) + h / 2.6;
-let sentence = axiom;
-let count = 0;
+const init = () => {
+  const html = document.getElementsByTagName("html").item(0),
+    canvas = document.getElementsByTagName("canvas").item(0),
+    c = canvas.getContext("2d");
 
-let bgc;
+  let flower;
+  const gui = new dat.GUI();
 
-const rules = [];
-rules[0] = {
-    a: 'F',
-    b: 'Go[+F][-F]GF*'
+  class Flower {
+    constructor(x, y, size) {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+    }
+
+    buildFlower() {
+      c.beginPath();
+      c.rect(
+        getResolution().w / 2 - this.size / 2,
+        getResolution().h / 2 - this.size / 2,
+        this.size,
+        this.size
+      );
+      c.stroke();
+    }
+  }
+
+  const resize = () => {
+    canvas.width = w = window.innerWidth;
+    canvas.height = h = window.innerHeight;
+    console.log(`screen resolution: ${w}px × ${h}px`);
+  };
+
+  const getResolution = () => {
+    return { w: canvas.width, h: canvas.height };
+  };
+
+  const setup = () => {
+    c.clearRect(0, 0, getResolution().w, getResolution().h);
+    flower = new Flower(getResolution().w, getResolution().h, 200);
+    gui.add(flower, "size", 0, 200);
+  };
+
+  setup();
+
+  const draw = (t) => {
+    c.fillStyle = "rgba(255, 255, 255, .5)";
+    c.fillRect(0, 0, w, h);
+
+    flower.buildFlower();
+
+    window.requestAnimationFrame(draw);
+  };
+
+  let w,
+    h,
+    last,
+    i = 0,
+    start = 0;
+
+  window.removeEventListener("load", init);
+  window.addEventListener("resize", resize);
+  resize();
+  window.requestAnimationFrame(draw);
 };
 
-rules[1] = {
-    a: 'G',
-    b: 'GG'
-};
-
-function setup() {
-    createCanvas(w, h);
-    if (baseColor.r + baseColor.g + baseColor.b <= 300){
-        bgc = [250, 250, 250];
-    } else {
-        bgc = [11];
-    }
-}
-
-function turtle() {
-   background(bgc);
-    angle = radians(Math.random() * (mainConfig.stems.angle.max - mainConfig.stems.angle.min) + mainConfig.stems.angle.min);
-    resetMatrix();
-    translate(width / 2, height - 15);
-    for (var i = 0; i < sentence.length; i++) {
-        strokeWeight(mainConfig.stems.strokeWeight);
-        stroke(mainConfig.stems.color.r, mainConfig.stems.color.g, mainConfig.stems.color.b);
-        var current = sentence.charAt(i);
-        if (current == 'F' || current == 'G') {
-            line(0, 0, 0, -len);
-            translate(0, -len);
-        } else if (current == '*') {
-            if (i % mainConfig.flowers.density === 0) {
-                scale(Math.random() * (len / (Math.random() * (mainConfig.flowers.scale.max - mainConfig.flowers.scale.min) + mainConfig.flowers.scale.min)));
-                makeFlower();
-            }
-        } else if (current == '+') {
-            let positiveRotation = angle * Math.random() * mainConfig.stems.randomSeed;
-            rotate(positiveRotation);
-        } else if (current == '-') {
-            let negativeRotation = -angle * Math.random() * mainConfig.stems.randomSeed;
-            rotate(negativeRotation);
-        } else if (current == '[') {
-            push();
-        } else if (current == ']') {
-            pop();
-            count++;
-        }
-    }
-    if (i >= sentence.length) {
-        finished = true;
-    }
-}
-
-function generateStems(iterations) {
-    for (i = iterations - 1; i > 0; i--) {
-        branch();
-    }
-}
-
-function branch() {
-    len *= Math.random() * (.52 - .45) + .45;
-    var nextSentence = '';
-    for (var i = 0; i < sentence.length; i++) {
-        var current = sentence.charAt(i);
-        var found = false;
-        for (var j = 0; j < rules.length; j++) {
-            if (current == rules[j].a) {
-                found = true;
-                nextSentence += rules[j].b;
-                break;
-            }
-        }
-        if (!found) {
-            nextSentence += current;
-        }
-    }
-    sentence = nextSentence;
-    turtle();
-}
-
-function draw() {
-    noFill();
-    smooth();
-    background(3, 3, 3);
-    createSingleFlower();
-    noLoop();
-}
-
-function createSingleFlower(){
-    turtle();
-    generateStems(mainConfig.stems.count);
-}
+window.addEventListener("load", init);
